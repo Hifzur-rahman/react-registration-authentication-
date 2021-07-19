a35978cfae41737207b072f9f6dc391d98125f6d
Hifzur Rahman <hifzurwebdeveloper@gmail.com>
2021-07-19 15:46:33 +0530

    config changes

--- src/config.js
+++ src/config.js
@@ -1,14 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
+// add your config file here
+
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyDsNf1oEP067Koe54EDegQB15z6BkhKN1Q",
-  authDomain: "react1-props.firebaseapp.com",
-  projectId: "react1-props",
-  storageBucket: "react1-props.appspot.com",
-  messagingSenderId: "519455695906",
-  appId: "1:519455695906:web:15129d7cad731eb06d967b",
-  measurementId: "G-1Z9T23YE0Y"
+  // apiKey: "",
+  // authDomain: "",
+  // projectId: "",
+  // storageBucket: "",
+  // messagingSenderId: "",
+  // appId: "",
+  // measurementId: ""
 });
 
 export default firebaseConfig;
